8c8b9fdef9284b40ac704e3714d29b0cc7d3ea47
bhargavmln <bhargavmln@gmail.com>
2020-11-02 19:31:55 +0530

    [bhargavmln]add:ability to retrieve data between given dates

--- src/main/java/com/capg/payrollservice/PayrollServiceDB.java
+++ src/main/java/com/capg/payrollservice/PayrollServiceDB.java
@@ -1,48 +1,56 @@
 package com.capg.payrollservice;
 
-import java.sql.*;
-import java.util.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PayrollServiceDB {
-	List<EmployeePayrollData> employeePayrollList;
 	EmployeePayrollData empDataObj = null;
-
-	public List<EmployeePayrollData> viewEmployeePayroll() throws DBServiceException {
-		employeePayrollList = new ArrayList<>();
+	
+	public List<EmployeePayrollData> viewEmployeePayroll() throws DBServiceException
+	{
+		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
 		String query = "select * from Employee_Payroll";
-		try (Connection con = new PayrollService().getConnection()) {
+		try(Connection con = new PayrollService().getConnection()) {
 			Statement statement = con.createStatement();
 			ResultSet resultSet = statement.executeQuery(query);
-			while (resultSet.next()) {
+			while(resultSet.next())
+			{
 				int id = resultSet.getInt(1);
 				String name = resultSet.getString(2);
 				String gender = resultSet.getString(3);
 				double salary = resultSet.getDouble(4);
 				LocalDate start = resultSet.getDate(5).toLocalDate();
-				empDataObj = new EmployeePayrollData(id, name, gender, salary, start);
-				employeePayrollList.add(empDataObj);
+				empDataObj = new EmployeePayrollData(id, name, gender ,salary,start);
+				employeePayrollList.add(empDataObj);	
+				
 			}
 		} catch (Exception e) {
 			throw new DBServiceException("SQL Exception", DBServiceExceptionType.SQL_EXCEPTION);
 		}
 		return employeePayrollList;
 	}
-
-	public List<EmployeePayrollData> viewEmployeePayrollByName(String name) throws DBServiceException {
+	
+	public List<EmployeePayrollData> viewEmployeePayrollByName(String name) throws DBServiceException
+	{
 		List<EmployeePayrollData> employeePayrollListByName = new ArrayList<>();
 		String query = "select * from Employee_Payroll where name = ?";
-		try (Connection con = new PayrollService().getConnection()) {
+		try(Connection con = new PayrollService().getConnection()) {
 			PreparedStatement preparedStatement = con.prepareStatement(query);
 			preparedStatement.setString(1, name );
 			ResultSet resultSet = preparedStatement.executeQuery();
-			if (resultSet.next()) {
+			if(resultSet.next())
+			{
 				int id = resultSet.getInt(1);
 				String gender = resultSet.getString(3);
 				double salary = resultSet.getDouble(4);
 				LocalDate start = resultSet.getDate(5).toLocalDate();
-				empDataObj = new EmployeePayrollData(id, name, gender, salary, start);
+				empDataObj = new EmployeePayrollData(id, name, gender ,salary,start);
 				employeePayrollListByName.add(empDataObj);
 			}
 		} catch (Exception e) {
@@ -50,47 +58,78 @@ public class PayrollServiceDB {
 		}
 		return employeePayrollListByName;
 	}
-
-	public void updateEmployeeSalary(String name, double salary) throws DBServiceException {
-		String query = String.format("update Employee_Payroll set salary = %.2f where name = '%s';", salary, name);
-		try (Connection con = new PayrollService().getConnection()) {
+	
+	public void updateEmployeeSalary(String name , double salary) throws DBServiceException
+	{
+		String query = String.format("update Employee_Payroll set salary = %.2f where name = '%s';", salary , name);
+		try(Connection con = new PayrollService().getConnection()) {
 			Statement statement = con.createStatement();
 			int result = statement.executeUpdate(query);
-			empDataObj = getEmployeePayrollData(name);
-			if (result > 0 && empDataObj != null)
+			empDataObj = getEmployeePayrollData( name);
+			if(result > 0 && empDataObj != null)
 				empDataObj.setSalary(salary);
-		} catch (Exception e) {
+		}catch (Exception e) {
 			throw new DBServiceException("SQL Exception", DBServiceExceptionType.SQL_EXCEPTION);
 		}
 	}
-
-	public void updateEmployeeSalaryUsingPreparedStatement(String name, double salary) throws DBServiceException {
+	
+	public void updateEmployeeSalaryUsingPreparedStatement(String name , double salary) throws DBServiceException
+	{
 		String query = "update Employee_Payroll set salary = ? where name = ?";
-		try (Connection con = new PayrollService().getConnection()) {
+		try(Connection con = new PayrollService().getConnection()) {
 			PreparedStatement preparedStatement = con.prepareStatement(query);
 			preparedStatement.setDouble(1, salary);
 			preparedStatement.setString(2, name);
 			int result = preparedStatement.executeUpdate();
-			empDataObj = getEmployeePayrollData(name);
-			if (result > 0 && empDataObj != null)
+			empDataObj = getEmployeePayrollData( name);
+			if(result > 0 && empDataObj != null)
 				empDataObj.setSalary(salary);
-		} catch (Exception e) {
+		}catch (Exception e) {
 			throw new DBServiceException("SQL Exception", DBServiceExceptionType.SQL_EXCEPTION);
 		}
+	}	
+	public EmployeePayrollData getEmployeePayrollData(String name) throws DBServiceException {
+		return viewEmployeePayroll().stream()
+								  .filter(e -> e.getName()
+								  .equals(name))
+								  .findFirst()
+								  .orElse(null);
 	}
-
-	private EmployeePayrollData getEmployeePayrollData(String name) {
-		return employeePayrollList.stream().filter(e -> e.getName().equals(name)).findFirst().orElse(null);
-	}
-
+	
 	public boolean isEmpPayrollSyncedWithDB(String name) throws DBServiceException {
-		boolean result = false;
 		try {
-			result = viewEmployeePayrollByName(name).get(0).equals(getEmployeePayrollData(name));
-		} catch (IndexOutOfBoundsException e) {
+			return viewEmployeePayrollByName(name).get(0).equals(getEmployeePayrollData(name));
+		} 
+		catch (IndexOutOfBoundsException e) {
+		} 
+		catch (Exception e) {
+			throw new DBServiceException("SQL Exception", DBServiceExceptionType.SQL_EXCEPTION);
+		}
+		return false;
+	}
+	
+	public List<EmployeePayrollData> viewEmployeePayrollByJoinDateRange(LocalDate startDate , LocalDate endDate) throws DBServiceException
+	{
+		List<EmployeePayrollData> employeePayrollListByStartDate = new ArrayList<>();
+		String query = "select * from Employee_Payroll where start_date between ? and  ?";
+		try(Connection con = new PayrollService().getConnection()) {
+			PreparedStatement preparedStatement = con.prepareStatement(query);
+			preparedStatement.setDate(1, Date.valueOf(startDate));
+			preparedStatement.setDate(2, Date.valueOf(endDate));
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while(resultSet.next())
+			{
+				int id = resultSet.getInt(1);
+				String name = resultSet.getString(2);
+				String gender = resultSet.getString(3);
+				double salary = resultSet.getDouble(4);
+				LocalDate start = resultSet.getDate(5).toLocalDate();
+				empDataObj = new EmployeePayrollData(id, name, gender ,salary,start);
+				employeePayrollListByStartDate.add(empDataObj);
+			}
 		} catch (Exception e) {
 			throw new DBServiceException("SQL Exception", DBServiceExceptionType.SQL_EXCEPTION);
 		}
-		return result;
+		return employeePayrollListByStartDate;
 	}
 }
--- src/test/java/com/capg/payrollservice/PayrollServiceTest.java
+++ src/test/java/com/capg/payrollservice/PayrollServiceTest.java
@@ -1,40 +1,47 @@
 package com.capg.payrollservice;
 
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
+import java.time.LocalDate;
 import java.util.List;
-
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PayrollServiceTest {
 	static PayrollServiceDB payrollService;
+
 	@BeforeClass
-	public static void setUp()  {
+	public static void setUp() {
 		payrollService = new PayrollServiceDB();
 	}
 
 	@Test
-	public void givenEmpPayrollDB_WhenRetrieved_ShouldMatchEmpCount() throws DBServiceException{
+	public void givenEmpPayrollDB_WhenRetrieved_ShouldMatchEmpCount() throws DBServiceException {
 		List<EmployeePayrollData> empPayrollList = payrollService.viewEmployeePayroll();
 		assertEquals(3, empPayrollList.size());
 	}
-	
+
 	@Test
-	public void givenUpdatedSalary_WhenRetrieved_ShouldBeSyncedWithDB() throws DBServiceException{
+	public void givenUpdatedSalary_WhenRetrieved_ShouldBeSyncedWithDB() throws DBServiceException {
 		payrollService.updateEmployeeSalary("Terisa", 3000000.0);
 		boolean isSynced = payrollService.isEmpPayrollSyncedWithDB("Terisa");
 		assertTrue(isSynced);
 	}
-	
+
 	@Test
-	public void givenUpdatedSalary_WhenRetrieved_ShouldBeSyncedWithDBUsingPreparedStatement() throws DBServiceException{
+	public void givenUpdatedSalary_WhenRetrieved_ShouldBeSyncedWithDBUsingPreparedStatement()
+			throws DBServiceException {
 		payrollService.updateEmployeeSalaryUsingPreparedStatement("Terisa", 3000000.0);
 		boolean isSynced = payrollService.isEmpPayrollSyncedWithDB("Terisa");
 		assertTrue(isSynced);
+	}
+
+	@Test
+	public void givenDateRange_WhenRetrieved_ShouldMatchEmpCount() throws DBServiceException {
+		List<EmployeePayrollData> empPayrollList = payrollService
+				.viewEmployeePayrollByJoinDateRange(LocalDate.of(2018,02,01), LocalDate.now());
+		assertEquals(2, empPayrollList.size());
 	}
 
 }
