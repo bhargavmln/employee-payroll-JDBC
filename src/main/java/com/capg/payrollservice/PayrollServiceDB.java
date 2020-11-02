64c5883e93987e63d389645b0d7f4e8d1da2d482
bhargavmln <bhargavmln@gmail.com>
2020-11-02 19:58:58 +0530

    [bhargavmln]add:ability to find min max sum etc.

--- src/main/java/com/capg/payrollservice/PayrollServiceDB.java
+++ src/main/java/com/capg/payrollservice/PayrollServiceDB.java
@@ -7,7 +7,9 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PayrollServiceDB {
 	EmployeePayrollData empDataObj = null;
@@ -131,5 +133,22 @@ public class PayrollServiceDB {
 			throw new DBServiceException("SQL Exception", DBServiceExceptionType.SQL_EXCEPTION);
 		}
 		return employeePayrollListByStartDate;
+	}
+	
+	public Map<String,Double> viewEmployeeDataGroupedByGender(String column , String operation) throws DBServiceException
+	{
+		Map<String,Double> empDataByGender = new HashMap<>();
+		String query = String.format("select gender , %s(%s) from Employee_Payroll group by gender;" , operation , column);
+		try(Connection con = new PayrollService().getConnection()) {
+			PreparedStatement preparedStatement = con.prepareStatement(query);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while(resultSet.next())
+			{
+				empDataByGender.put(resultSet.getString(1), resultSet.getDouble(2));
+			}
+		}catch (Exception e) {
+			throw new DBServiceException("SQL Exception", DBServiceExceptionType.SQL_EXCEPTION);
+		}
+		return empDataByGender;
 	}
 }
--- src/test/java/com/capg/payrollservice/PayrollServiceTest.java
+++ src/test/java/com/capg/payrollservice/PayrollServiceTest.java
@@ -1,25 +1,32 @@
 package com.capg.payrollservice;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PayrollServiceTest {
 	static PayrollServiceDB payrollService;
-
+	static Map<String, Double> empDataByGender;
+	static List<EmployeePayrollData> empPayrollList;
+	
 	@BeforeClass
-	public static void setUp() {
+	public static void setUp()  {
 		payrollService = new PayrollServiceDB();
+		empDataByGender = new HashMap<>();
+		empPayrollList = new ArrayList<>();
 	}
 
 	@Test
 	public void givenEmpPayrollDB_WhenRetrieved_ShouldMatchEmpCount() throws DBServiceException {
 		List<EmployeePayrollData> empPayrollList = payrollService.viewEmployeePayroll();
-		assertEquals(3, empPayrollList.size());
+		Assert.assertEquals(3, empPayrollList.size());
 	}
 
 	@Test
@@ -41,7 +48,42 @@ public class PayrollServiceTest {
 	public void givenDateRange_WhenRetrieved_ShouldMatchEmpCount() throws DBServiceException {
 		List<EmployeePayrollData> empPayrollList = payrollService
 				.viewEmployeePayrollByJoinDateRange(LocalDate.of(2018,02,01), LocalDate.now());
-		assertEquals(2, empPayrollList.size());
+		Assert.assertEquals(2, empPayrollList.size());
+	}
+	
+	@Test
+	public void givenEmployeeDB_WhenRetrievedSum_ShouldReturnSumGroupedByGender() throws DBServiceException {
+		empDataByGender = payrollService.viewEmployeeDataGroupedByGender("salary" , "sum");
+		Assert.assertEquals(400000, empDataByGender.get("M"),0.0);
+		Assert.assertEquals(3000000, empDataByGender.get("F"),0.0);
+	}
+
+	@Test
+	public void givenEmployeeDB_WhenRetrievedAvg_ShouldReturnAvgByGroupedGender() throws DBServiceException {
+		empDataByGender = payrollService.viewEmployeeDataGroupedByGender("salary" , "avg");
+		Assert.assertEquals(200000, empDataByGender.get("M"),0.0);
+		Assert.assertEquals(3000000, empDataByGender.get("F"),0.0);
+	}
+
+	@Test
+	public void givenEmployeeDB_WhenRetrievedMax_ShouldReturnMaxGroupedByGender() throws DBServiceException {
+		empDataByGender = payrollService.viewEmployeeDataGroupedByGender("salary" , "max");
+		Assert.assertEquals(300000, empDataByGender.get("M"),0.0);
+		Assert.assertEquals(3000000, empDataByGender.get("F"),0.0);
+	}
+	
+	@Test
+	public void givenEmployeeDB_WhenRetrievedMin_ShouldReturnMinGroupedByGender() throws DBServiceException {
+		empDataByGender = payrollService.viewEmployeeDataGroupedByGender("salary" , "min");
+		Assert.assertEquals(100000, empDataByGender.get("M"),0.0);
+		Assert.assertEquals(3000000, empDataByGender.get("F"),0.0);
+	}
+
+	@Test
+	public void givenEmployeeDB_WhenRetrievedCount_ShouldReturnCountGroupedByGender() throws DBServiceException {
+		empDataByGender = payrollService.viewEmployeeDataGroupedByGender("salary", "count");
+		Assert.assertEquals(2, empDataByGender.get("M"),0.0);
+		Assert.assertEquals(1, empDataByGender.get("F"),0.0);
 	}
 
 }
